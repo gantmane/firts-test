41e3528809d709b4f271cf71daf40d661b546709
gantmane <92798015+gantmane@users.noreply.github.com>
2022-12-21 17:29:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// подключение express
 const express = require("express");
-// создаем объект приложения
+ 
 const app = express();
-// определяем обработчик для маршрута "/"
 app.get("/", function(request, response){
      
-    // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+    response.send("<h1>Главная страница</h1>");
 });
-// начинаем прослушивать подключения на 3000 порту
+app.get("/about", function(request, response){
+     
+    response.send("<h1>О сайте</h1>");
+});
+app.get("/contact", function(request, response){
+     
+    response.send("<h1>Контакты</h1>");
+});
 app.listen(3000);
